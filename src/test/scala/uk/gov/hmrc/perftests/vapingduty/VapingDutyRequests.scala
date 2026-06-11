c7b010212174e37fa804d7b32f5fc23ea2121c56
SamuelBarima <93492001+SamuelTwumBarima@users.noreply.github.com>
2026-06-11 14:31:31 +0100

    update url

--- src/test/scala/uk/gov/hmrc/perftests/vapingduty/VapingDutyRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/vapingduty/VapingDutyRequests.scala
@@ -126,7 +126,7 @@ object VapingDutyRequests extends ServicesConfiguration {
     s"$dutySuspendedPath/suspended-products"
 
   private val dutySuspendedCYAUrl: String =
-    s"$dutySuspendedPath/check-your-answers"
+    s"$completeReturnPath/duty-suspense-check-answers"
 
   private val enterDutySuspenseUrl: String =
     s"$dutySuspendedPath/enter-received-or-moved-amount"
