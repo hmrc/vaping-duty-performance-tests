6f4559421fcb29d4734fc458352e2a6a781306c0
SamuelBarima <93492001+SamuelTwumBarima@users.noreply.github.com>
2026-06-25 11:20:11 +0100

    view payments

--- src/test/scala/uk/gov/hmrc/perftests/vapingduty/VapingDutyRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/vapingduty/VapingDutyRequests.scala
@@ -137,6 +137,10 @@ object VapingDutyRequests extends ServicesConfiguration {
   private val enterSpoiltAmountUrl          = s"$adjustmentPath/enter-spoilt-amount"
   private val addAnotherSpoiltAdjustmentUrl = s"$adjustmentPath/add-another-spoilt-adjustment"
 
+  // ---------- View Payments URLs ----------
+  val viewPaymentsUrl: String =
+    s"$vapingDutyPath/view-payments"
+
   def saveCsrfToken(): CheckBuilder[RegexCheckType, String] = regex(_ => CsrfPattern).saveAs("csrfToken")
 
   def randomTestEmail(): String = {
@@ -480,6 +484,12 @@ object VapingDutyRequests extends ServicesConfiguration {
   val getDutySuspendedCYAPage: HttpRequestBuilder =
     http("Get Duty Suspended Check Your Answers Page")
       .get(s"$dutySuspendedCYAUrl?period=#{period}")
+      .check(status.is(200))
+      .check(saveCsrfToken())
+
+  val getViewPaymentsPage: HttpRequestBuilder =
+    http("Get View Your Returns Page")
+      .get(viewPaymentsUrl)
       .check(status.is(200))
       .check(saveCsrfToken())
 
